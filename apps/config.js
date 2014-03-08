fe1f8edc2633017f46fe0cb0883150d49b3c4ea2
Prateek Bhatt <prattbhatt@gmail.com>
2014-03-08 21:46:42 +0530

    Added apps/config.js which will contain configs to be used by all apps; Updated installation shell scripts

---
+++ apps/config.js
@@ -0,0 +1,11 @@
+/**
+ * Config settings spanning across all apps
+ */
+
+exports.development = {
+
+  websiteHost: 'http://do',
+  dashboardHost: 'http://app.do',
+  apiHost: 'http://api.do'
+
+};
--- setup/install-npm-global-dependencies.sh
+++ setup/install-npm-global-dependencies.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+set -e
+
+SAILS_VERSION=0.10.0-rc4
+
 echo 'Installing npm global dependencies ...'
 
-echo 'Installing bower ...'
+echo 'Installing bower globally ...'
 npm install bower -g
 
 # install sails latest version
-echo 'Installing sails 0.10.0-rc4 ...'
-npm install sails@0.10.0-rc4 -g
+echo 'Installing sails' $SAILS_VERSION 'globally ...'
+npm install sails@$SAILS_VERSION -g
 
-echo 'Installing node pm2 ...'
+echo 'Installing pm2 globally ...'
 npm install pm2 -g
 
 exit 0
