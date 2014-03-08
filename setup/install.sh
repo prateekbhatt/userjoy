21fb96a790ccd8721a511d05ec6df4b05315fa5c
Prateek Bhatt <prattbhatt@gmail.com>
2014-03-09 00:26:42 +0530

    Removed line changing permission of run.sh file from install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -21,12 +21,9 @@ bash ./setup/install-npm-project-dependencies.sh
 
 bash ./setup/install-apps.sh
 
-# make run.sh executable
-sudo chmod u+x ./run.sh
-
 # create symlink of bin/dodatado.js in /usr/loca/bin/dodatado
 sudo ln -sf $(pwd)/bin/dodatado.js /usr/local/bin/dodatado
 
-bash ./setup/print-instructions.sh
+echo '    Type "dodatado start" in your command line to start application'
 
 exit 0
