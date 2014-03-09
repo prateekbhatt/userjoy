c77f9137d103826afccfbcd8d484dbb013e7e88a
Prateek Bhatt <prattbhatt@gmail.com>
2014-03-09 13:25:06 +0530

    Refactored config.js; Added hosts.js to each app to expose the base domains of all apps

--- apps/config.js
+++ apps/config.js
@@ -2,10 +2,36 @@
  * Config settings spanning across all apps
  */
 
-exports.development = {
+var path = require('path'),
+  _ = require('lodash'),
+  config = {};
 
-  websiteHost: 'http://do',
-  dashboardHost: 'http://app.do',
-  apiHost: 'http://api.do'
-
+config.ports = {
+  website: 8000,
+  dashboard: 8001,
+  api: 8002
 };
+
+config.baseUrl = {
+  development: 'do.localhost',
+  production: 'dodatado.com'
+};
+
+
+/**
+ * Hostnames for each app
+ * e.g., in production environment:
+ * dashboard: app.dodatado.com
+ */
+config.hosts = {};
+_.each(config.baseUrl, function (url, env) {
+
+  config.hosts[env] = {
+    website: 'http://' + url,
+    dashboard: 'http://' +'app.' + url,
+    api: 'http://' +'api.' + url
+  };
+
+});
+
+module.exports = config;
