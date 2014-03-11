778212598ff59f626f215d8b5f32904198928f2d
Prateek Bhatt <prattbhatt@gmail.com>
2014-03-11 15:44:29 +0530

    Updated apps/config.js file to return a config object by taking the environment and appName as params Updated local.js files accordingly Removed hosts.js files as they are not required anymore Added Readme to the apps/ folder

--- apps/website/config/hosts.js
+++
@@ -1,9 +0,0 @@
-/**
- * Hosts
- *
- * Hosts are the domains for each of the apps
- * They are defined in the apps/config.js file
- */
-
-var env = process.env.NODE_ENV;
-module.exports.hosts = require('../../config').hosts[env];
