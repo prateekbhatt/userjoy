21b34caf4069ca7a3a428300a6a4345ccb5e8c82
Prateek Bhatt <prattbhatt@gmail.com>
2014-03-11 15:44:29 +0530

    Updated apps/config.js file to return a config object by taking the environment and appName as params Updated local.js files accordingly Removed hosts.js files as they are not required anymore Added Readme to the apps/ folder

--- apps/config.js
+++ apps/config.js
@@ -3,35 +3,52 @@
  */
 
 var path = require('path'),
-  _ = require('lodash'),
-  config = {};
-
-config.ports = {
-  website: 8000,
-  dashboard: 8001,
-  api: 8002
-};
-
-config.baseUrl = {
-  development: 'do.localhost',
-  production: 'dodatado.com'
-};
-
+  _ = require('lodash');
 
 /**
- * Hostnames for each app
- * e.g., in production environment:
- * dashboard: app.dodatado.com
+ * General config settings for all apps
+ * @param  {string} env     environment in which the app is started
+ * @param  {string} appName name of the application
+ * @return {object}         config settings object
  */
-config.hosts = {};
-_.each(config.baseUrl, function (url, env) {
+module.exports = function (env, appName) {
 
-  config.hosts[env] = {
-    website: 'http://' + url,
-    dashboard: 'http://' +'app.' + url,
-    api: 'http://' +'api.' + url
+  var config = {};
+
+  var ports = {
+    website: 8000,
+    dashboard: 8001,
+    api: 8002
   };
 
-});
+  config.port = ports[appName];
 
-module.exports = config;
+  config.environment = env;
+
+  var baseUrl = {
+    development: 'do.localhost',
+    production: 'dodatado.com'
+  };
+
+  config.baseUrl = baseUrl[env];
+
+  /**
+   * Hostnames for each app
+   * e.g., in production environment:
+   * dashboard: app.dodatado.com
+   */
+  config.hosts = {};
+
+  (function (url) {
+
+    config.hosts = {
+      website: 'http://' + url,
+      dashboard: 'http://' + 'app.' + url,
+      api: 'http://' + 'api.' + url
+    };
+
+  })(config.baseUrl);
+
+  return config;
+
+};
