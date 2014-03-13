1170424404b5e681f73d0b208029554b05932b6a
savinay <savinay.90@gmail.com>
2014-03-13 18:39:17 +0530

    Added functionalities for the message app

--- apps/dashboard/assets/js/app/controllers/Message.js
+++ apps/dashboard/assets/js/app/controllers/Message.js
@@ -8,7 +8,7 @@ angular.module('do.message', [])
                     views: {
                         "main": {
                             templateUrl: '/templates/message.html',
-                            controller: 'MessageCtrl'
+                            controller: 'InboxCtrl'
                         }
                     }
                 })
@@ -16,7 +16,8 @@ angular.module('do.message', [])
                     url: '/inbox',
                     views: {
                         "messageapp": {
-                            templateUrl: '/templates/message.inbox.html'
+                            templateUrl: '/templates/message.inbox.html',
+                            controller: 'InboxCtrl'
                         }
                     }
                 })
@@ -24,18 +25,47 @@ angular.module('do.message', [])
                     url: '/sent',
                     views: {
                         "messageapp": {
-                            templateUrl: '/templates/message.sent.html'
+                            templateUrl: '/templates/message.sent.html',
+                            controller: 'SentCtrl'
+                        }
+                    }
+
+                })
+                .state('message.compose', {
+                    url: '/compose',
+                    views: {
+                        "messageapp": {
+                            templateUrl: '/templates/message.compose.html'
+                        }
+                    }
+
+                }).state('message.compose.automate', {
+                    url: '/automate',
+                    views: {
+                        "automatemsg": {
+                            templateUrl: '/templates/message.compose.automate.html'
+                        }
+                    }
+
+                }).state('message.compose.automate.write', {
+                    url: '/write',
+                    views: {
+                        "automatemsgwrite": {
+                            templateUrl: '/templates/message.compose.automate.write.html'
                         }
                     }
 
                 });
 
 
+
         }
     ])
 
-    .controller('MessageCtrl', ['$scope', '$filter', 'ngTableParams',
+    .controller('InboxCtrl', ['$scope', '$filter', 'ngTableParams',
         function MessageCtrl($scope, $filter, ngTableParams) {
+            
+
             var data = [{
                 name: 'Larry Page',
                 subject: 'Hi, Thanks for such an offer. I really....',
@@ -106,7 +136,7 @@ angular.module('do.message', [])
                 time: '3:29 PM'
             }];
             
-            $scope.columns = [{
+            $scope.columnsInbox = [{
                 title: 'User',
                 field: 'name',
                 visible: true,
@@ -123,7 +153,7 @@ angular.module('do.message', [])
                 visible: true
             }];
 
-            $scope.tableParams = new ngTableParams({
+            $scope.tableParamsInbox = new ngTableParams({
                 page: 1, // show first page
                 count: 10, // count per page
                 filter: {
@@ -147,6 +177,171 @@ angular.module('do.message', [])
                     params.total(orderedData.length); // set total for recalc paginationemail
 
                     $defer.resolve(orderedData.slice((
+                            params.page() -
+                            1) * params.count(),
+                        params.page() *
+                        params.count()));
+                }
+            });
+        }
+    ])
+
+    .controller('SentCtrl', ['$scope', '$filter', 'ngTableParams',
+        function MessageCtrl($scope, $filter, ngTableParams) {
+
+            var data = [{
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larry Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }, {
+                name: 'Larro Page',
+                subject: 'Hi, Thanks for such an offer. I really....',
+                time: '3:29 PM',
+                opened: 'Yes',
+                replied: 'No'
+            }];
+            
+            $scope.columnsSent = [{
+                title: 'User',
+                field: 'name',
+                visible: true,
+                filter: {
+                    'name': 'text'
+                }
+            }, {
+                title: 'Text',
+                field: 'subject',
+                visible: true
+            }, {
+                title: 'When',
+                field: 'time',
+                visible: true
+            }, {
+                title: 'Opened',
+                field: 'opened',
+                visible:'true'
+            },{
+                title: 'Replied',
+                field: 'replied',
+                visible: 'true'
+            }];
+
+            $scope.tableParamsSent = new ngTableParams({
+                page: 1, // show first page
+                count: 10, // count per page
+                filter: {
+                    name: 'M' // initial filter
+                },
+                sorting: {
+                    name: 'asc'
+                }
+            }, {
+                total: data.length, // length of data
+                getData: function ($defer, params) {
+                    // use build-in angular filter
+                    var filteredData = params.filter() ?
+                        $filter('filter')(data, params
+                            .filter()) :
+                        data;
+                    var ordereddata = params.sorting() ?
+                        $filter('orderBy')(data,
+                            params.orderBy()) :
+                        data;
+                    params.total(ordereddata.length); // set total for recalc paginationemail
+
+                    $defer.resolve(ordereddata.slice((
                             params.page() -
                             1) * params.count(),
                         params.page() *
