44328e27e13ade490b81a9100cb3bba277176971
savinay <savinay.90@gmail.com>
2014-04-11 15:31:47 +0530

    Added jquery validation to signup form. Password minimum length is 5. This change needs to be made in the backend

--- apps/website/assets/js/signup.js
+++ apps/website/assets/js/signup.js
@@ -1,23 +1,28 @@
-$('#signup_form_submit').click(function (e) {
-  e.preventDefault();
-  // FIXME: change url for production
-  $.ajax({
-    url: 'http://api.do.localhost/account',
-    type: 'POST',
-    data: $('#signup_form').serialize(),
-    dataType: 'json',
-    success: function (data) {
-      console.log('signup success', arguments);
-      //redirect to login
-    },
-    error: function (error) {
-      console.log('signup error', arguments, '\n', JSON.parse(error.responseText).error);
-      displayError(JSON.parse(error.responseText).error);
-      //show error
-    }
-  })
+$('#signup_form_submit')
+    .click(function (e) {
+        e.preventDefault();
+        // TODO: change url for production
+        $.ajax({
+            url: 'http://api.do.localhost/account',
+            type: 'POST',
+            data: $('#signup_form')
+                .serialize(),
+            dataType: 'json',
+            success: function (data) {
+                console.log('signup success', arguments);
+                //redirect to login
+            },
+            error: function (error) {
+                console.log('signup error', arguments, '\n', JSON.parse(
+                        error.responseText)
+                    .error);
+                displayError(JSON.parse(error.responseText)
+                    .error);
+                //show error
+            }
+        })
 
-});
+    });
 
 /**
  * Add jquery validation to valdiate form elements: TODO
@@ -26,8 +31,39 @@ $('#signup_form_submit').click(function (e) {
 
 // })
 
+$(function () {
+        console.log("inside validate jquery");
+        $("#signup_form").validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                }
+            },
+
+            messages: {
+                email: {
+                    required: "Please enter your email"
+                },
+                password: {
+                    required: "Please provide a password",
+                    minlength: "Your password must be atleast 5 characters long"
+                }
+            },
+
+            submitHandler: function(form) {
+                form.submit();
+            }
+        });
+    });
+
 function displayError(err) {
-  console.log("inside display error");
-  $("#signupalert").css("display", "block");
-  $('#errortext').text(err);
+    $("#signupalert")
+        .css("display", "block");
+    $('#errortext')
+        .text(err);
 }
