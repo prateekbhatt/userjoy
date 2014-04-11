8a5e9ea05861e602ab54b4d4524ce84e79a2a07d
savinay <savinay.90@gmail.com>
2014-04-11 23:14:34 +0530

    Text Angular editor height is now adjustable

--- apps/dashboard/assets/js/app/controllers/Message.js
+++ apps/dashboard/assets/js/app/controllers/Message.js
@@ -452,6 +452,7 @@ angular.module('do.message', [])
 
 .controller('textAngularCtrl', ['$scope',
     function ($scope) {
+        // $scope.htmlVariable = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li style="color: blue;">Super Easy <b>Theming</b> Options</li><li>Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li>Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE8+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p>';
         $scope.taRegisterTool = {
             textAngularEditors: {
                 demo1: {
@@ -516,7 +517,7 @@ angular.module('do.message', [])
                         name: "createLink",
                         title: "Create Link"
                     }],
-                    html: "<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li>Super Easy <b>Theming</b> Options</li><li>Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li>Doesn't Use an iFrame</li><li>Works with Firefox, Chrome, and IE10+</li></ol><p><b>Code at GitHub:</b> <a href='https://github.com/fraywing/textAngular'>Here</a> </p>",
+                    htmlVariable: '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li style="color: blue;">Super Easy <b>Theming</b> Options</li><li>Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li>Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE8+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p>',
                     disableStyle: false,
                     theme: {
                         editor: {
