28a6727accff1047db162278950fca42b6dc8e27
Prateek Bhatt <prattbhatt@gmail.com>
2014-04-15 16:52:50 +0530

    api: add EventSchema as embedded document in Session Model
    add SessionModel#addEvent method to push new events into
    an existing session
    
    update tests

--- apps/api/test/unit/models/Session.test.js
+++ apps/api/test/unit/models/Session.test.js
@@ -20,16 +20,18 @@ describe('Model Session', function () {
   describe('#create', function () {
 
 
-    it('should return error if appId is not present', function (done) {
+    it('should return error if aid is not present', function (done) {
 
       var newSession = {
-        platform: 'Desktop',
-        userId: randomId
+        pl: 'Desktop',
+        uid: randomId
       };
 
       Session.create(newSession, function (err, sess) {
         expect(err)
           .to.exist;
+        expect(err.errors.aid.message)
+          .to.eql('Invalid aid');
         expect(sess)
           .not.to.exist;
         done();
@@ -37,16 +39,18 @@ describe('Model Session', function () {
     });
 
 
-    it('should return error if userId is not present', function (done) {
+    it('should return error if uid is not present', function (done) {
 
       var newSession = {
         platform: 'Desktop',
-        appId: randomId
+        aid: randomId
       };
 
       Session.create(newSession, function (err, sess) {
         expect(err)
           .to.exist;
+        expect(err.errors.uid.message)
+          .to.eql('Invalid uid');
         expect(sess)
           .not.to.exist;
         done();
@@ -54,13 +58,13 @@ describe('Model Session', function () {
     });
 
 
-    it('should create new session if appId and userId are present',
+    it('should create new session if aid and uid are present',
       function (done) {
 
         var newSession = {
-          platform: 'Desktop',
-          appId: randomId,
-          userId: randomId
+          pl: 'Desktop',
+          aid: randomId,
+          uid: randomId
         };
 
         Session.create(newSession, function (err, sess) {
@@ -68,7 +72,7 @@ describe('Model Session', function () {
             .to.not.exist;
           expect(sess)
             .to.be.an('object');
-          expect(sess.platform)
+          expect(sess.pl)
             .to.eql('Desktop');
 
           savedSession = sess;
@@ -78,16 +82,165 @@ describe('Model Session', function () {
 
       });
 
-    it('should add createdAt to new session', function () {
+    it('should add ct (created) timestamp to new session', function () {
       expect(savedSession)
-        .to.have.property('createdAt');
+        .to.have.property('ct');
     });
 
-    it('should not add updatedAt timestamp to new session',
+    it('should add ut (updated) timestamp to new session',
       function () {
         expect(savedSession)
-          .not.to.have.property('updatedAt');
+          .to.have.property('ut');
       });
+
+    it('should add events to ev embedded document if present', function (
+      done) {
+      var newSession = {
+        pl: 'Desktop',
+        aid: randomId,
+        uid: randomId,
+        ev: [{
+          t: 'pageview',
+          d: 'app.dodatado.com',
+          p: '/login',
+          n: 'Login Page',
+          f: 'Authentication'
+        }]
+      };
+
+      Session.create(newSession, function (err, sess) {
+        expect(err)
+          .to.not.exist;
+
+        expect(sess.ev)
+          .to.have.length(1);
+
+        expect(sess.ev[0].n)
+          .to.eql('Login Page');
+
+        done();
+      });
+    });
+
+
+    it('should return error if event document does not have domain',
+      function (done) {
+        var newSession = {
+          pl: 'Desktop',
+          aid: randomId,
+          uid: randomId,
+          ev: [{
+            t: 'pageview',
+            p: '/login',
+            n: 'Login Page',
+            f: 'Authentication'
+          }]
+        };
+
+        Session.create(newSession, function (err, sess) {
+          expect(err)
+            .to.exist;
+
+          expect(sess)
+            .to.not.exist;
+
+          expect(err.errors['ev.0.d'].message)
+            .to.eql('Invalid domain');
+
+          done();
+        });
+      });
+
+
+    it('should return error if event document does not have path',
+      function (done) {
+        var newSession = {
+          pl: 'Desktop',
+          aid: randomId,
+          uid: randomId,
+          ev: [{
+            t: 'pageview',
+            d: 'dodatado.com',
+            n: 'Login Page',
+            f: 'Authentication'
+          }]
+        };
+
+        Session.create(newSession, function (err, sess) {
+          expect(err)
+            .to.exist;
+
+          expect(sess)
+            .to.not.exist;
+
+          expect(err.errors['ev.0.p'].message)
+            .to.eql('Invalid path');
+
+          done();
+        });
+      });
+
+
+    it(
+      'should return error if event type is not in ["feature", "pageview"]',
+      function (done) {
+        var newSession = {
+          pl: 'Desktop',
+          aid: randomId,
+          uid: randomId,
+          ev: [{
+            t: 'randomType',
+            d: 'dodatado.com',
+            p: '/login',
+            n: 'Login Page',
+            f: 'Authentication'
+          }]
+        };
+
+        Session.create(newSession, function (err, sess) {
+          expect(err)
+            .to.exist;
+
+          expect(sess)
+            .to.not.exist;
+
+          expect(err.errors['ev.0.t'].message)
+            .to.eql("Event type must be one of 'pageview' or 'feature'");
+
+          done();
+        });
+      });
+
+  });
+
+  describe('#newEvent', function () {
+
+    it('should add a new event to an existing session', function (done) {
+
+      var sid = savedSession._id;
+      var event = {
+        t: 'feature',
+        p: '/users',
+        d: 'dodatado.com',
+        f: 'Users',
+        n: 'Segment Users'
+      };
+
+      Session.newEvent(sid, event, function (err, sess) {
+
+        expect(err)
+          .to.not.exist;
+
+        expect(sess.ev)
+          .to.have.length(1);
+
+        expect(sess.ev[0].n)
+          .to.eql('Segment Users');
+
+        done();
+      });
+
+    });
 
   });
 
