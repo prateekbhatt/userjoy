e8b671829944e3d3b6d3305bc0db6f39d251c6f8
Prateek Bhatt <prattbhatt@gmail.com>
2014-05-04 15:50:32 +0530

    api: add route to ConversationController to fetch all conversations of an app

--- apps/api/test/unit/controllers/ConversationController.test.js
+++ apps/api/test/unit/controllers/ConversationController.test.js
@@ -20,6 +20,7 @@ describe('Resource /apps/:aid/conversations', function () {
    */
 
   var Conversation = require('../../../api/models/Conversation');
+  var Message = require('../../../api/models/Message');
 
 
   var newConversation = {
@@ -151,6 +152,101 @@ describe('Resource /apps/:aid/conversations', function () {
               .to.be.false;
           })
           .end(done);
+
+      });
+
+  });
+
+  describe('GET /apps/:aid/conversations/:coId', function () {
+
+    var testCon;
+    var testPath;
+
+    before(function (done) {
+      testCon = saved.conversations.first;
+      testPath = basePath + '/' + testCon._id;
+      logoutUser(done);
+    });
+
+
+    it('should return error if not logged in', function (done) {
+
+      request
+        .get(testPath)
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .expect({
+          status: 401,
+          error: 'Unauthorized'
+        })
+        .end(done);
+
+    });
+
+
+    it('logging in user',
+
+      function (done) {
+        loginUser(done);
+      });
+
+
+    it('should return all messages belonging to message thread',
+
+      // TODO: make sure atleast two messages are there in the thread
+
+      function (done) {
+
+        request
+          .get(testPath)
+          .set('cookie', loginCookie)
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function (err, res) {
+
+            expect(res.body)
+              .to.be.an("object");
+
+            expect(res.body.messages)
+              .to.be.an("array");
+
+            expect(res.body.messages)
+              .to.have.length.above(0);
+
+            done();
+          });
+
+      });
+
+
+    // this test depends on the output of the previous test, hence it should be
+    // below
+    it(
+      'should update seen status to true for all messages, in the thread, sent from user',
+      function (done) {
+
+        Message
+          .find({
+            coId: testCon._id
+          })
+          .exec(function (err, msgs) {
+
+            if (err) return done(err);
+
+            expect(msgs)
+              .to.have.length.above(0);
+
+            _.chain(msgs)
+              .each(function (m) {
+                if (m.from === 'user') {
+                  expect(m.seen)
+                    .to.be.true;
+                }
+              })
+              .value();
+
+            done();
+          });
 
       });
 
