2de795a1196b8af61e1b138da304d2f53d0d07b7
Prateek Bhatt <prattbhatt@gmail.com>
2014-05-07 14:01:50 +0530

    api: add metadata helper

---
+++ apps/api/test/unit/helpers/metadata.test.js
@@ -0,0 +1,30 @@
+describe.only('Helper metadata', function () {
+
+  var metadata = require('../../../helpers/metadata');
+
+  var exMetadata = {
+    status: 'pending',
+    tries: 991
+  };
+
+  var expectedOutput = [{
+    k: 'status',
+    v: 'pending'
+  }, {
+    k: 'tries',
+    v: 991
+  }];
+
+  it('should return an array of key-value pairs', function () {
+    var a = metadata.format(exMetadata);
+    expect(a)
+      .to.eql(expectedOutput);
+  });
+
+
+  it('should return empty array if no input', function () {
+    expect(metadata.format())
+      .to.eql([]);
+  });
+
+});
