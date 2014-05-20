1ef756c2801d53cccf0328dade3326d1387b1825
Prateek Bhatt <prattbhatt@gmail.com>
2014-05-20 11:25:11 +0530

    api: moved Invite routes from AppController to new InviteController renamed endpoint from /invite to /invites in the load/index.js file, the InviteController route MUST be placed on top of all /apps routes, because it has got a route which does not required the isAuthenticated or hasAccess policies

---
+++ apps/api/test/unit/controllers/InviteController.test.js
@@ -0,0 +1,229 @@
+describe('Resource /apps/:aid/invites', function () {
+
+  /**
+   * npm dependencies
+   */
+
+  var mongoose = require('mongoose');
+  var randomId = mongoose.Types.ObjectId;
+
+
+  /**
+   * Models
+   */
+
+  var Invite = require('../../../api/models/Invite');
+
+
+
+  before(function (done) {
+    setupTestDb(done);
+  });
+
+
+  describe('POST /apps/:aid/invites', function () {
+
+    var testUrl;
+
+    before(function (done) {
+      testUrl = '/apps/' + saved.apps.first._id + '/invites';
+      logoutUser(done);
+    });
+
+    it('should return error if not logged in',
+
+      function (done) {
+
+        request
+          .post(testUrl)
+          .send({})
+          .expect('Content-Type', /json/)
+          .expect(401)
+          .end(done);
+
+      });
+
+
+    it('logging in user', function (done) {
+      loginUser(done);
+    });
+
+
+    it('should create and send new invite',
+
+      function (done) {
+
+        var newInvite = {
+          name: 'Prats',
+          email: 'prattbhatt@gmail.com'
+        };
+
+        request
+          .post(testUrl)
+          .send(newInvite)
+          .set('cookie', loginCookie)
+          .expect('Content-Type', /json/)
+          .expect(201)
+          .end(function (err, res) {
+
+            expect(res.body)
+              .to.be.an("object");
+
+            expect(res.body)
+              .to.have.property("toEmail", newInvite.email);
+
+            expect(res.body)
+              .to.have.property("toName", newInvite.name);
+
+            done(err);
+          });
+
+      });
+
+  });
+
+
+  describe('GET /apps/:aid/invites/:inviteId', function () {
+
+    var testUrl;
+    var testInvite;
+
+    before(function (done) {
+      testInvite = saved.invites.first;
+      testUrl = '/apps/' + saved.apps.first._id + '/invites/' +
+        testInvite._id;
+      logoutUser(done);
+    });
+
+
+    // NOTE: the saved invite has been created using the saved account.
+    // Hence, it would not through the 'Create Account' error.
+    it('should confirm invitation',
+
+      function (done) {
+        request
+          .get(testUrl)
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function (err, res) {
+
+            expect(res.body)
+              .to.be.an("object");
+
+            expect(res.body)
+              .to.have.property("message", 'REDIRECT_TO_LOGIN');
+
+            expect(res.body)
+              .to.have.property("success", true);
+
+            done(err);
+          });
+
+      });
+
+
+    // NOTE: the following test must be below the previous test, because we are checking
+    // if the invite fixture has been deleted from the db after the confirmation
+    // in the previous test
+    it('should delete invite confirmed', function (done) {
+      Invite.findById(testInvite._id, function (err, inv) {
+
+        expect(err)
+          .to.not.exist;
+
+        expect(inv)
+          .to.not.exist;
+
+        done();
+      })
+    });
+
+
+    it(
+      'should return error and delete invite, if the account is already in the team',
+
+      function (done) {
+
+        var inviteId;
+
+        var testUrl = '/apps/' + saved.apps.first._id + '/invites/';
+
+        Invite.create(
+
+          {
+            aid: saved.apps.first._id,
+            from: saved.accounts.second._id,
+            toEmail: saved.accounts.first.email,
+            toName: 'Random Name'
+          },
+
+          function (err, inv) {
+
+            if (err) return done(err);
+            inviteId = inv._id;
+            testUrl = testUrl + inviteId;
+
+            request
+              .get(testUrl)
+              .expect('Content-Type', /json/)
+              .expect(200)
+              .end(function (err, res) {
+
+                expect(res.body)
+                  .to.be.an("object");
+
+                expect(res.body)
+                  .to.have.property("message", 'IS_TEAM_MEMBER');
+
+                expect(res.body)
+                  .to.have.property("success", false);
+
+                Invite.findById(inviteId, function (err, deletedInv) {
+
+                  expect(err)
+                    .to.not.exist;
+
+                  expect(deletedInv)
+                    .to.not.exist;
+
+                  done();
+                });
+
+              });
+
+          });
+
+
+      });
+
+
+    it('should return error if invite not found',
+      function (done) {
+
+        var testUrl = '/apps/' + saved.apps.first._id + '/invites/' +
+          randomId();
+
+        request
+          .get(testUrl)
+          .expect('Content-Type', /json/)
+          .expect(404)
+          .end(function (err, res) {
+
+            expect(res.body)
+              .to.be.an("object");
+
+            expect(res.body)
+              .to.have.property("message", 'INVITE_NOT_FOUND');
+
+            expect(res.body)
+              .to.have.property("success", false);
+
+            done(err);
+          });
+
+      });
+
+
+  });
+
+});
