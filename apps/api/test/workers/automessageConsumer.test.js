5377b096cd043bb9e310f445be5257b36e5c180e
Prateek Bhatt <prattbhatt@gmail.com>
2014-05-24 23:41:18 +0530

    api: add automessageConsumer to poll messages from the queue and run queries and send mails/notifications

---
+++ apps/api/test/workers/automessageConsumer.test.js
@@ -0,0 +1,263 @@
+describe('Worker automessageConsumer', function () {
+
+
+  /**
+   * npm dependencies
+   */
+
+  var moment = require('moment');
+  var mongoose = require('mongoose');
+
+
+  /**
+   * Models
+   */
+
+  var AutoMessage = require('../../api/models/AutoMessage');
+  var Event = require('../../api/models/Event');
+  var Notification = require('../../api/models/Notification');
+
+
+  /**
+   * Workers
+   */
+
+  var worker = require('../../workers/automessageConsumer');
+
+
+  /**
+   * Iron mq Queue
+   */
+
+  var queue = worker._queue;
+
+
+  /**
+   * Test variables
+   */
+
+  var randomId = mongoose.Types.ObjectId;
+
+
+  before(function (done) {
+
+    async.series([
+
+      function (cb) {
+        setupTestDb(cb);
+      },
+
+
+      function clearQueue(cb) {
+        queue.clear(cb);
+      },
+
+
+      function createEvent(cb) {
+        var aid = saved.automessages.first.aid;
+        var uid = saved.users.first._id;
+
+        var newEvent = {
+          aid: aid,
+          uid: uid,
+          type: 'feature',
+          name: 'Create Notification'
+        };
+
+        Event.create(newEvent, cb);
+      }
+
+    ], done);
+
+  });
+
+  describe('#amConsumer type:email', function () {
+
+    var amId;
+
+    before(function (cb) {
+      amId = saved.automessages.first._id.toString();
+
+      // queue auto message
+      queue.post(amId, cb);
+    });
+
+
+    it('should fetch amId and send automessages', function (done) {
+
+      worker._amConsumer(function (err, queueId, automessage) {
+
+        expect(err)
+          .to.not.exist;
+
+        expect(queueId)
+          .to.be.a("string")
+          .that.is.not.empty;
+
+        expect(automessage)
+          .to.be.an("object")
+          .that.is.not.empty;
+
+        done();
+      });
+
+    });
+
+
+    it('should create new automessage queue event', function (done) {
+
+      var query = {
+        type: 'automessage',
+        meta: {
+          $all: [
+
+            {
+              $elemMatch: {
+                k: 'amId',
+                v: amId.toString()
+              }
+            },
+
+            {
+              $elemMatch: {
+                k: 'state',
+                v: 'queued'
+              }
+            }
+
+          ]
+        }
+      };
+
+      Event
+        .find(query)
+        .exec(function (err, evn) {
+
+          expect(err)
+            .to.not.exist;
+
+          expect(evn)
+            .to.be.an('array')
+            .that.has.length(1);
+
+          expect(evn[0])
+            .to.have.property('meta')
+            .that.is.an('array');
+
+          done();
+        });
+
+    });
+
+
+
+  });
+
+
+  describe('#amConsumer type:notification', function () {
+
+    var amId;
+
+    before(function (cb) {
+
+      amId = saved.automessages.second._id.toString();
+
+      // queue auto message
+      queue.post(amId, cb);
+    });
+
+    it('should fetch amId and send automessages', function (done) {
+
+      worker._amConsumer(function (err, queueId, automessage) {
+
+        expect(err)
+          .to.not.exist;
+
+        expect(queueId)
+          .to.be.a("string")
+          .that.is.not.empty;
+
+        expect(automessage)
+          .to.be.an("object")
+          .that.is.not.empty;
+
+        done();
+      });
+
+    });
+
+
+    it('should create new automessage queue event', function (done) {
+
+      var query = {
+        type: 'automessage',
+        meta: {
+          $all: [
+
+            {
+              $elemMatch: {
+                k: 'amId',
+                v: amId.toString()
+              }
+            },
+
+            {
+              $elemMatch: {
+                k: 'state',
+                v: 'queued'
+              }
+            }
+
+          ]
+        }
+      };
+
+      Event
+        .find(query)
+        .exec(function (err, evn) {
+
+          expect(err)
+            .to.not.exist;
+
+          expect(evn)
+            .to.be.an('array')
+            .that.has.length(1);
+
+          expect(evn[0])
+            .to.have.property('meta')
+            .that.is.an('array');
+
+          done();
+        });
+
+    });
+
+
+    it('should create new notification to be shown to the user',
+      function (done) {
+
+        Notification
+          .find({
+            amId: amId
+          })
+          .exec(function (err, notf) {
+            console.log(err, notf);
+            expect(err)
+              .to.not.exist;
+
+            expect(notf)
+              .to.be.an("array")
+              .that.has.length(1);
+
+            expect(notf[0].amId.toString())
+              .to.equal(amId.toString());
+
+            done();
+          });
+
+      });
+
+  });
+
+
+});
---
+++ apps/api/workers/automessageConsumer.js
@@ -0,0 +1,384 @@
+/**
+ * This worker pulls automessages that need to be run from the queue, and
+ * then runs them
+ */
+
+
+/**
+ * npm dependencies
+ */
+
+var _ = require('lodash');
+var async = require('async');
+var cronJob = require('cron')
+  .CronJob;
+var iron_mq = require('iron_mq');
+var moment = require('moment');
+
+
+/**
+ * Helpers
+ */
+
+var getMailerLocals = require('../helpers/get-mailer-locals');
+var logger = require('../helpers/logger');
+
+
+/**
+ * Lib
+ */
+
+var Query = require('../api/lib/query');
+
+
+/**
+ * Models
+ */
+
+var AutoMessage = require('../api/models/AutoMessage');
+var Event = require('../api/models/Event');
+var Notification = require('../api/models/Notification');
+var Segment = require('../api/models/Segment');
+
+
+/**
+ * Services
+ */
+
+var mailer = require('../api/services/mailer');
+
+
+/**
+ * Config settings
+ *
+ * TODO: move these to central config settings file
+ */
+
+var TOKEN = 'Rfh192ozhicrSZ2R9bDX8uRvOu0';
+var PROJECT_ID_DEV = '536e5455bba6150009000090';
+
+
+/**
+ * Hourly Cron Job
+ */
+
+var HOURLY_SCHEDULE = '0 * * * *';
+
+
+/**
+ * Per Minute Cron
+ */
+
+var MINUTE_SCHEDULE = '*/1 * * * *';
+
+
+// TODO: THIS CODE NEEDS TO BE MANAGED IN INSIDE THE APPS CONFIG FILE
+var SCHEDULE = MINUTE_SCHEDULE;
+if (process.env.NODE_ENV === 'production') {
+  SCHEDULE = HOURLY_SCHEDULE;
+}
+
+
+/**
+ * Create iron mq client instance
+ */
+
+var imq = new iron_mq.Client({
+  token: TOKEN,
+  project_id: PROJECT_ID_DEV,
+  queue_name: 'automessage'
+});
+
+
+/**
+ * use 'automessge' queue on iron mq
+ */
+
+var q = imq.queue("automessage");
+
+
+
+function saveNotifications(users, amsg, cb) {
+
+  var aid = amsg.aid;
+  var amId = amsg._id;
+  var body = amsg.body;
+  var sender = amsg.creator.name;
+  var title = amsg.title;
+
+  var iterator = function (u, cb) {
+
+    var uid = u._id;
+
+    // FIXME: render body
+
+    var n = {
+      amId: amId,
+      body: body,
+      sender: sender,
+      uid: uid
+    };
+
+    // save the new notification
+    Notification.create(n, function (err) {
+
+      if (err) return cb(err);
+
+      // ids object (required to create an 'automessage' event)
+      var ids = {
+        aid: aid,
+        amId: amId,
+        uid: uid
+      };
+
+      // create 'automessage' event
+      Event.automessage(ids, 'queued', title, cb);
+
+    });
+
+  };
+
+  async.each(users, iterator, cb);
+}
+
+
+function amConsumer(cb) {
+
+  // the iron mq message id (required to delete the message from the queue)
+  var queueMsgId;
+  var automessage;
+
+
+  async.waterfall(
+
+    [
+
+      function getFromQueue(cb) {
+
+        // get one message at a time
+        var opts = {
+          n: 1
+        };
+
+        q.get(opts, function (err, res) {
+
+          logger.trace({
+            at: 'workers/automessageConsumer getFromQueue',
+            err: err,
+            res: res
+          });
+
+          if (err) return cb(err);
+
+          // store the queue message id
+          queueMsgId = res.id;
+
+          // the message body contains the automessage id
+          if (!res.body) {
+            return cb(new Error('Automessage id not found in queue'));
+          }
+
+          cb(null, res.body);
+        });
+      },
+
+
+      function findAutoMessage(autoMessageId, cb) {
+
+        AutoMessage
+          .findById(autoMessageId)
+          .populate('creator')
+          .exec(function (err, amsg) {
+
+            logger.trace({
+              at: 'workers/automessageConsumer findAutoMessage',
+              err: err,
+              amsg: amsg
+            });
+
+            if (err) return cb(err);
+            if (!amsg) return cb(new Error('AutoMessage not found'));
+
+            // store automessage in a local variable
+            automessage = amsg;
+
+            cb(null, automessage);
+          });
+      },
+
+
+      function findSegment(automessage, cb) {
+
+        Segment
+          .findById(automessage.sid)
+          .exec(function (err, seg) {
+            if (err) return cb(err);
+            if (!seg) return cb(new Error('Segment not found'));
+            cb(null, seg);
+          });
+      },
+
+
+      function runQuery(segment, cb) {
+
+        logger.trace({
+          at: 'workers/automessageConsumer runQuery',
+          segment: segment
+        });
+
+        // segment object should be converted from BSON to JSON
+        segment = segment.toJSON();
+
+        var query = {
+          list: segment.list,
+          op: segment.op,
+          filters: segment.filters
+        };
+
+        var query = new Query(segment.aid, query);
+
+        query.run(function (err, users) {
+          cb(err, users);
+        });
+
+      },
+
+
+      // NOTE: if type is email, send the emails
+      // else if type is notification, create notifications to be shown later
+      //
+      //
+      // FIXME: Need to store which users are being sent the mails / notifications
+      // in order to prevent double-sending mails etc.
+      //
+      // This can be achieved by creating a new type of event in the Event
+      // collection which will identify the user and automessage alongwith the
+      // action (sent, seen, clicked, replied)
+
+      function sendEmails(users, cb) {
+
+        // if message type is not email, skip this
+        if (automessage.type !== "email") return cb(null, users);
+
+        logger.trace({
+          at: 'workers/automessageConsumer sendEmails',
+          users: users
+        });
+
+        async.each(
+
+          users,
+
+          function iterator(u, cb) {
+
+            var locals = getMailerLocals('auto', automessage, u.name, u.email);
+
+            mailer.sendAutoMessage(locals, function (err) {
+              if (err) return cb(err);
+
+              var ids = {
+                aid: automessage.aid,
+                amId: automessage._id,
+                uid: u._id
+              };
+
+              var state = 'queued';
+              var title = automessage.title;
+
+              Event.automessage(ids, state, title, function (err, evn) {
+                cb(err, evn)
+              });
+
+            });
+          },
+
+          function callback(err) {
+
+            if (err) {
+
+              logger.crit({
+                at: 'amConsumer:send',
+                err: err,
+                amId: automessage._id
+              });
+            }
+
+            cb(err, users);
+          });
+      },
+
+
+      function sendNotifications(users, cb) {
+
+
+        // if message type is not notification, skip this
+        if (automessage.type !== "notification") return cb();
+
+        logger.trace({
+          at: 'workers/automessageConsumer sendNotifications',
+          users: users
+        });
+
+        saveNotifications(users, automessage, cb);
+
+      },
+
+
+      function deleteFromQueue(cb) {
+
+        q.del(queueMsgId, function (err, body) {
+
+          logger.trace({
+            at: 'workers/automessageConsumer deleteFromQueue',
+            queueMsgId: queueMsgId,
+            err: err,
+            body: body
+          });
+
+          cb(err);
+
+        });
+      }
+
+    ],
+
+    function finalCallback(err) {
+      cb(err, queueMsgId, automessage);
+    }
+  )
+}
+
+
+module.exports = function run() {
+
+  async.forever(
+
+    function foreverFunc(next) {
+
+      amConsumer(function (err) {
+        setImmediate(next);
+      });
+      // setImmediate(function () {})
+    },
+
+    function foreverCallback(err) {
+
+      logger.crit({
+        at: 'amConsumer async.forever',
+        err: err,
+        time: Date.now()
+      });
+
+    }
+  );
+
+}
+
+
+
+/**
+ * Expose function for test cases
+ */
+
+module.exports._amConsumer = amConsumer;
+module.exports._queue = q;
