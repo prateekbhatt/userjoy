f793b1d07567c54e5cc06e7f37e79f466a55f2eb
Prateek Bhatt <prattbhatt@gmail.com>
2014-06-04 20:02:20 +0530

    api: add /track/conversations route to create conversation from the feedback widget

--- apps/api/api/routes/TrackController.js
+++ apps/api/api/routes/TrackController.js
@@ -4,6 +4,7 @@
 
 var _ = require('lodash');
 var async = require('async');
+var cors = require('cors');
 var router = require('express')
   .Router();
 
@@ -13,6 +14,7 @@ var router = require('express')
  */
 
 var App = require('../models/App');
+var Conversation = require('../models/Conversation');
 var Notification = require('../models/Notification');
 var User = require('../models/User');
 
@@ -29,6 +31,13 @@ var track = require('../lib/track');
  */
 
 var logger = require('../../helpers/logger');
+
+
+/**
+ * Add CORS support for all /track routes
+ */
+
+router.use(cors());
 
 
 /**
@@ -300,10 +309,17 @@ router
   .route('/notifications')
   .get(function (req, res, next) {
 
+
+    logger.trace({
+      at: 'TrackController:getNotification',
+      query: req.query,
+      cookies: req.cookies
+    });
+
+
     var data = req.query;
     var appKey = data.app_id;
     var mode;
-
 
     // Validations
     if (!appKey) {
@@ -325,13 +341,6 @@ router
     mode = appKey.split('_')[0];
 
 
-
-    logger.trace({
-      at: 'TrackController:getNotification',
-      query: req.query,
-    });
-
-
     async.waterfall([
 
 
@@ -342,7 +351,9 @@ router
 
         function getOrCreateUser(app, cb) {
 
-          var conditions = {};
+          var conditions = {
+            aid: app._id
+          };
 
           if (user_id) {
             conditions.user_id = user_id;
@@ -384,6 +395,122 @@ router
         res
           .status(200)
           .json(notification);
+
+      });
+
+
+  });
+
+
+
+/**
+ * POST /track/conversations
+ *
+ * Creates a new conversation
+ */
+
+router
+  .route('/conversations')
+  .post(function (req, res, next) {
+
+    logger.trace({
+      at: 'TrackController:createConversation',
+      body: req.body,
+      cookies: req.cookies
+    });
+
+    var data = req.body;
+    var appKey = data.app_id;
+    var body = data.body;
+    var mode;
+
+    // Validations
+    if (!appKey) {
+      return res.badRequest('Please send app_id with the params');
+    }
+
+    // user identifiers
+    var email = data.email;
+    var user_id = data.user_id;
+
+    // if user identifier is not present, respond with an error
+    if (!email && !user_id) {
+      return res.badRequest('Please send user_id or email to identify user');
+    }
+
+    // is message body is not present, send bad request error
+    if (!body) {
+      return res.badRequest('Please write a message');
+    }
+
+    // check if the request is in test / live mode
+    mode = appKey.split('_')[0];
+
+
+    async.waterfall([
+
+
+        function getApp(cb) {
+          App.findByKey(mode, appKey, cb);
+        },
+
+
+        function getOrCreateUser(app, cb) {
+
+          var conditions = {
+            aid: app._id
+          };
+
+          if (user_id) {
+            conditions.user_id = user_id;
+          } else if (email) {
+            conditions.email = email;
+          } else {
+            return cb(new Error('Please send user_id or email'));
+          }
+
+          User.findOne(conditions, cb);
+        },
+
+
+        function createConversation(user, cb) {
+
+          var newConversation = {
+            aid: user.aid,
+            messages: [],
+            sub: 'Message from ' + user.email,
+            uid: user._id
+          };
+
+          var msg = {
+            body: body,
+
+            // add from as 'user'
+            from: 'user',
+
+            type: 'email',
+
+            // add sender name 'sName' as user's name or email
+            // TODO: Check is user.name will work
+            sName: user.name || user.email
+
+          };
+
+          newConversation.messages.push(msg);
+
+          Conversation.create(newConversation, cb);
+        }
+
+      ],
+
+
+      function callback(err, conversation) {
+
+        if (err) return next(err);
+
+        res
+          .status(201)
+          .json(conversation);
 
       });
 
--- apps/api/test/unit/controllers/TrackController.test.js
+++ apps/api/test/unit/controllers/TrackController.test.js
@@ -340,4 +340,124 @@ describe('Resource /track', function () {
   });
 
 
+  describe('POST /track/conversations', function () {
+
+    var url = '/track/conversations';
+    var appKey;
+
+    before(function (done) {
+      appKey = saved.apps.first.liveKey;
+      logoutUser(done);
+    });
+
+    it('should return error if there is no app_id', function (done) {
+
+      request
+        .post(url)
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .expect({
+          status: 400,
+          error: 'Please send app_id with the params'
+        })
+        .end(done);
+    });
+
+    it('should return error if there is no user_id or email',
+      function (done) {
+
+        var testUrl = url;
+        var newCon = {
+          'app_id': appKey
+        };
+
+        request
+          .post(testUrl)
+          .send(newCon)
+          .expect('Content-Type', /json/)
+          .expect(400)
+          .expect({
+            status: 400,
+            error: 'Please send user_id or email to identify user'
+          })
+          .end(done);
+
+      });
+
+    it('should return error if there is no body',
+      function (done) {
+
+        var testUrl = url;
+        var newCon = {
+          'app_id': appKey,
+          'email': saved.users.first.email
+        };
+
+        request
+          .post(testUrl)
+          .send(newCon)
+          .expect('Content-Type', /json/)
+          .expect(400)
+          .expect({
+            status: 400,
+            error: 'Please write a message'
+          })
+          .end(done);
+
+      });
+
+    it('should create new conversation',
+      function (done) {
+
+        var email = saved.users.first.email;
+        var testUrl = url;
+        var newCon = {
+          'app_id': appKey,
+          'email': saved.users.first.email,
+          'body': 'Hey man, how are you?'
+        };
+
+        request
+          .post(testUrl)
+          .send(newCon)
+          .expect('Content-Type', /json/)
+          .expect(201)
+          .end(function (err, res) {
+
+            if (err) return done(err);
+
+            var notf = res.body;
+
+            expect(notf)
+              .to.be.an('object');
+
+            var savedMsg = notf.messages[0];
+
+            expect(savedMsg)
+              .to.have.property("body", newCon.body);
+
+            expect(savedMsg)
+              .to.have.property("ct");
+
+            expect(savedMsg)
+              .to.have.property("seen");
+
+            expect(savedMsg)
+              .to.have.property("sName");
+
+            expect(notf)
+              .to.have.property("uid");
+
+            expect(notf)
+              .to.have.property("uid");
+
+
+            done();
+          });
+
+      });
+
+  });
+
+
 });
