24191c0314a518a3a885b119f55b801c12ead1ed
Prateek Bhatt <prattbhatt@gmail.com>
2014-06-06 11:22:15 +0530

    cdn: pass sender local var to notification template; insert notification template using 'dom' component

--- apps/cdn/lib/notification.js
+++ apps/cdn/lib/notification.js
@@ -1,6 +1,9 @@
 var ajax = require('ajax');
 var bind = require('bind');
+var debug = require('debug')('uj:notification');
+var dom = require('dom');
 var template = require('./notification-template');
+var user = require('./user');
 
 
 /**
@@ -8,129 +11,150 @@ var template = require('./notification-template');
  */
 
 function Notification() {
-    this.notification_url = 'TODO';
-    this.notification_template_id = 'uj_notification';
-    this.userId = null;
+
+  this.debug = debug;
+  this.notification_url = 'TODO';
+  this.automessageId;
+  this.uid;
+
+  this.TEMPLATE_ID = 'uj_notification';
+  this.FETCH_URL = 'http://api.do.localhost/track/notifications';
+  this.REPLY_POST_URL = 'http://api.do.localhost/track/notifications/uid';
 }
 
 
-Notification.prototype.load = function (userId) {
+Notification.prototype.load = function () {
 
-    var self = this;
+  var self = this;
 
+  this.debug('load');
 
-    self.userId = userId;
+  self.fetch(function (err, notf) {
 
-    self.fetch(function (err, notf) {
-        function create(htmlStr) {
-            var frag = document.createDocumentFragment(),
-                temp = document.createElement('div');
-            temp.innerHTML = htmlStr;
-            while (temp.firstChild) {
-                frag.appendChild(temp.firstChild);
-            }
-            return frag;
-        }
-
-        var locals = {
-            msg: 'This is the message to be shown to the user',
-            notification_template_id: self.notification_template_id
-        };
-
-        var fragment = create(template(locals));
+    // If there is an error, it should be logged during debugging
+    if (err) {
+      self.debug('err: ' + err);
+      return;
+    }
 
 
+    // If no notification found, do not anything
+    if (!notf) {
+      return;
+    }
 
-        // You can use native DOM methods to insert the fragment:
-        document.body.insertBefore(fragment, document.body.childNodes[0]);
-    });
+
+    // Persist the automessageId and the uid from the notification obj.
+    // Would be needed to create a new conversation in case the user
+    // replies back
+    self.automessageId = notf.amId;
+    self.uid = notf.uid;
+
+
+    // Create locals object which would be passed to render the template
+    var locals = {
+      sender: notf.sender,
+      body: notf.body,
+      TEMPLATE_ID: self.TEMPLATE_ID
+    };
+
+
+    // render the template with the locals, and insert it into the body
+    dom('body')
+      .prepend(template(locals));
+  });
 
 };
 
 Notification.prototype.fetch = function (cb) {
-    // FIXME
-    var dummy = {
-        userId: this.userId,
-        body: 'gjghjgjhg'
-    };
+
+  var self = this;
+
+  var userTraits = user.traits();
+
+  // get uid from cookie
+  var uid = user.id();
+
+  // unique user id as given by app
+  var user_id = userTraits.user_id;
+
+  // email of user
+  var email = userTraits.email;
+
+  // if no user identifier, return
+  if (!uid && !user_id && !email) {
+    self.debug('no user identifier');
+    return;
+  }
+
+  var conditions = {
+    app_id: window._userjoy_id
+  };
+
+  if (user_id) {
+    conditions.user_id = user_id;
+  } else if (email) {
+    conditions.email = email
+  }
 
 
-    return cb(null, dummy);
+  ajax({
+    type: 'GET',
+    url: self.FETCH_URL,
+    data: conditions,
+    success: function (msg) {
+      self.debug("success: ", msg);
+      cb(null, msg);
+    },
+    error: function (err) {
+      self.debug("error", err);
+      cb(err);
+    }
+  });
+
 }
 
 
-Notification.prototype.show = function () {
-    ajax({
-        type: 'GET',
-        url: 'api.do.localhost/track/notifications',
-        success: function (data) {
-            console.log("success: ", data);
-        },
-        error: function () {
-            console.log("error");
-        }
-    })
-};
+Notification.prototype.show = function () {};
 
 
 Notification.prototype.hide = function () {
-    document.getElementById(this.notification_template_id)
-        .style.display = 'none';
+  var id = this.TEMPLATE_ID;
+
+  this.debug('hide', dom(id));
+
+  document.getElementById(id)
+    .style.display = 'none';
+
 };
 
 
 Notification.prototype.reply = function () {
-    // var xhReq = new XMLHttpRequest();
-    // xhReq.onreadystatechange = function () {
 
-    // }
-    var data = {
-        reply: document.getElementById('reply')
-            .value
-    }
-    console.log("data: ", data);
-    ajax({
-        type: "POST",
-        url: '/apps/aid/notification/uid',
-        data: data,
-        success: function () {
-            document.getElementById('msgsent')
-            .style.display = 'block';
-        document.getElementById('reply')
-            .value = '';
-        },
-        error: function () {
-            console.log("error");
-        },
-        dataType: 'json'
-    });
-    // var request = new XMLHttpRequest();
-    // url = '';
-    // request.open("POST", 'url', true);
-    // request.send(document.getElementById('reply').value);
-    // console.log("request: ", request);
-    // console.log("readyState: ", request.readyState, request.status);
-    // request.onreadystatechange = function () {
-    //     console.log("inside onreadystatechange");
-    //     // request.setRequestHeader("Content-Type", "application/json");
-    //     if (request.readyState != 4 || request.status != 200) {
-    //         console.log("error");
-    //         return;
-    //     };
-    //     console.log("Success: " + request.responseText);
-    //     document.getElementById('msgsent')
-    //         .style.display = 'block';
-    //     document.getElementById('reply')
-    //         .value = '';
-    // }
-    // r.open("POST", "/userjoy/:aid/notification/:uid", true);
-    // r.onreadystatechange = function () {
-    // };
-    // xhReq.open("GET", "sumGet.phtml?figure1=5&figure2=10", true);
-    // xhReq.send(null);
-    // var serverResponse = xhReq.responseText;
-    // console.log(serverResponse);
+  var self = this;
 
+  var data = {
+    reply: dom('#reply')
+      .val()
+  };
+
+  this.debug("reply", data);
+
+  ajax({
+    type: "POST",
+    url: self.REPLY_POST_URL,
+    data: data,
+    success: function () {
+      document.getElementById('msgsent')
+        .style.display = 'block';
+      document.getElementById('reply')
+        .value = '';
+    },
+    error: function () {
+      console.log("error");
+    },
+    dataType: 'json'
+  });
 };
 
 
