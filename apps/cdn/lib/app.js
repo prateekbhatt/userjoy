1c82cdac9e13c0ff6c2c20428dc9fd7e0f0baf95
Prateek Bhatt <prattbhatt@gmail.com>
2014-06-11 19:31:51 +0530

    cdn: remove cookie/store default names from lib/app,company,user

--- apps/cdn/lib/app.js
+++ apps/cdn/lib/app.js
@@ -1,23 +1,8 @@
 
+var bind = require('bind');
 var debug = require('debug')('uj:app');
 var Entity = require('./entity');
 var inherit = require('inherit');
-var bind = require('bind');
-var cookie = require('./cookie');
-
-
-/**
- * App defaults
- */
-
-App.defaults = {
-  cookie: {
-    key: 'userjoy_app_id'
-  },
-  localStorage: {
-    key: 'userjoy_app_traits'
-  }
-};
 
 
 /**
@@ -27,7 +12,7 @@ App.defaults = {
  */
 
 function App (options) {
-  this.defaults = App.defaults;
+  this.defaults = {}
   this.debug = debug;
   Entity.call(this, options);
 }
