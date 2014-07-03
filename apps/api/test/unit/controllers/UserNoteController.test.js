bd313dba8ff886f0243d625bb1630b7c289949dd
Prateek Bhatt <prattbhatt@gmail.com>
2014-07-03 17:35:19 +0530

    api: populate note creator when a new usernote is created

--- apps/api/test/unit/controllers/UserNoteController.test.js
+++ apps/api/test/unit/controllers/UserNoteController.test.js
@@ -1,4 +1,4 @@
-describe('Resource /apps/:aid/usernotes', function () {
+describe('Resource /apps/:aid/users/:uid/notes', function () {
 
   /**
    * npm dependencies
@@ -161,7 +161,9 @@ describe('Resource /apps/:aid/usernotes', function () {
             .to.have.property("aid", aid.toString());
 
           expect(res.body)
-            .to.have.property("creator");
+            .to.have.property("creator")
+            .that.is.an('object')
+            .and.has.keys(['_id', 'name']);
 
           expect(res.body)
             .to.have.property("note", newNote.note);
@@ -178,7 +180,7 @@ describe('Resource /apps/:aid/usernotes', function () {
   });
 
 
-describe('PUT /apps/:aid/users/:uid/notes/:nid', function () {
+  describe('PUT /apps/:aid/users/:uid/notes/:nid', function () {
 
     var testUrl, uid, aid, nid;
     var testUserNote;
