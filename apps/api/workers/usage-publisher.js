fb6cf5965e4209d6c0163c3d7cc46077b72a1799
Prateek Bhatt <prattbhatt@gmail.com>
2014-07-05 22:32:08 +0530

    api: usage-publisher now updates the App#queuedUsage timestamp

--- apps/api/workers/usage-publisher.js
+++ apps/api/workers/usage-publisher.js
@@ -22,6 +22,7 @@ var q = require('./queues')
  */
 
 var logger = require('../helpers/logger');
+var QueueError = require('../helpers/queue-error');
 
 
 /**
@@ -42,7 +43,7 @@ var HOURLY_SCHEDULE = '0 * * * *';
  * Per Minute Cron
  */
 
-var TEN_MINUTE_SCHEDULE = '*/10 * * * *';
+var FIVE_MINUTE_SCHEDULE = '*/5 * * * *';
 
 
 /**
@@ -53,27 +54,47 @@ var DAILY_SCHEDULE = '0 0 * * *';
 
 
 // TODO: THIS CODE NEEDS TO BE MANAGED IN INSIDE THE APPS CONFIG FILE
-var SCHEDULE = TEN_MINUTE_SCHEDULE;
+var SCHEDULE = FIVE_MINUTE_SCHEDULE;
 if (process.env.NODE_ENV === 'production') {
   SCHEDULE = DAILY_SCHEDULE;
 }
 
 
 /**
- * Find all active apps
+ * Find all active apps that have not already been queued for given time
  *
+ * @param {date} updateTime start-of-day-updateTime
  * @param {function} cb callback
  */
 
-function findActiveApps(cb) {
+function findActiveApps(updateTime, cb) {
 
   App
     .find({
-      isActive: true
+      isActive: true,
+
+      $or: [
+
+        // if it has never been queued
+        {
+          queuedUsage: {
+            $exists: false
+          }
+        },
+
+        // if it has not been queued for the given time yet
+        {
+          queuedUsage: {
+            $lt: updateTime
+          }
+        }
+
+      ]
     })
     .select({
       _id: 1
     })
+    .lean()
     .exec(cb);
 
 }
@@ -90,45 +111,81 @@ function cronFunc(cb) {
   console.log('\n\n\n');
   logger.trace('usagePublisher:Started');
 
+  // update yesterday's usage
+  var updateTime = moment.utc()
+    .subtract('days', 1)
+    .startOf('day')
+    .valueOf();
+
   async.waterfall([
 
       function find(cb) {
-        findActiveApps(cb);
+        findActiveApps(updateTime, function (err, apps) {
+
+          if (err) return cb(err);
+
+          if (!_.isObject(apps) || _.isEmpty(apps)) {
+            return cb(new QueueError('NO_APPS_FOUND_FOR_QUEUEING'));
+          }
+
+          cb(null, apps);
+        });
       },
 
       function queue(apps, cb) {
 
-        var timestamp = moment()
-          .valueOf();
+        var aids = _.pluck(apps, '_id');
+        var appsData = _.map(aids, function (id) {
 
-        var appsData = _.chain(apps)
-          .pluck('_id')
-          .map(function (id) {
+          // add timestamp to app data
+          var a = {
+            aid: id.toString(),
+            updateTime: updateTime
+          };
 
-            // add timestamp to app data
-            var a = {
-              aid: id.toString(),
-              time: timestamp
-            };
-
-            // iron mq only accepts strings
-            return JSON.stringify(a);
-          })
-          .value();
+          // iron mq only accepts strings
+          return JSON.stringify(a);
+        });
 
         q()
           .post(appsData, function (err, queueIds) {
+            cb(err, aids, queueIds, appsData);
+          });
+      },
+
+
+      function updateQueuedTimestamp(aids, queueIds, appsData, cb) {
+
+        var query = {
+          _id: {
+            $in: aids
+          }
+        };
+
+        var update = {
+          queuedUsage: updateTime
+        };
+
+        var options = {
+          multi: true
+        };
+
+
+        App
+          .update(query, update, options, function (err, numberAffected) {
             cb(err, queueIds, appsData);
           });
       }
 
     ],
 
-    function finalCallback(err, queueIds, ids, numberAffected) {
+    function finalCallback(err, queueIds, appsData, numberAffected) {
 
       var logObj = {
         at: 'usagePublisher:Completed',
         err: err,
+        queueIds: queueIds,
+        appsData: appsData,
         ts: Date.now()
       };
 
@@ -136,7 +193,7 @@ function cronFunc(cb) {
 
 
       if (cb) {
-        return cb(err, queueIds, ids, numberAffected);
+        return cb(err, queueIds, appsData, numberAffected);
       }
 
     });
