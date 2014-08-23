5adfd946845f0312ef7e1b8c4e6c7a4140948a2d
savinay <savinay.90@gmail.com>
2014-08-23 19:36:34 +0530

    Added production.js and production.css

--- apps/dashboard/build/jst.39ba8e50.js
+++ apps/dashboard/build/jst.39ba8e50.js
@@ -54,7 +54,7 @@ this["JST"]["assets/templates/demo/demo.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<p></p>\n';
+__p += '<div class="col-md-offset-3 col-md-6 text-center">\n  <h3>Taking you to demo</h3>\n  <div class="progress">\n  <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">\n    <span class="sr-only">20% Complete</span>\n  </div>\n</div>\n</div>\n';
 
 }
 return __p
